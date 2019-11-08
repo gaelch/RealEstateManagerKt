a7e4eb570f72bbb8eec26c716596b868c64f592a
gaelch <zz-gael@hotmail.fr>
2019-11-08 14:57:07 +0100

    implémentation toolbar + menu toolbar + navigationDrawer

--- app/src/main/java/com/cheyrouse/gael/realestatemanagerkt/controllers/MainActivity.kt
+++ app/src/main/java/com/cheyrouse/gael/realestatemanagerkt/controllers/MainActivity.kt
@@ -4,18 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.cheyrouse.gael.realestatemanagerkt.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         configureToolbar()
+        configureNavDrawer()
+        configureNavView()
     }
 
     private fun configureToolbar() {
+        setSupportActionBar(toolbar)
+    }
 
+    private fun configureNavDrawer() {
+        drawer = activity_main_drawer_layout
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setHomeButtonEnabled(true)
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
+    }
+
+    private fun configureNavView() {
+        val navigationView: NavigationView = nav_view
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,17 +56,47 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.menu_search -> {
                 // Open search fragment
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.menu_edit-> {
                 // Open edit fragment
+                Toast.makeText(this, "edit", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.menu_create -> {
                 // Open create fragment
+                Toast.makeText(this, "create", Toast.LENGTH_SHORT).show()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.activity_main_drawer_simulator -> {
+                Toast.makeText(this, "simulator", Toast.LENGTH_SHORT).show()
+            }
+            R.id.activity_main_drawer_create -> {
+                Toast.makeText(this, "create", Toast.LENGTH_SHORT).show()
+            }
+            R.id.activity_main_drawer_edit -> {
+                Toast.makeText(this, "edit", Toast.LENGTH_SHORT).show()
+            }
+            R.id.activity_main_drawer_search -> {
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show()
+            }
+        }
+        activity_main_drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (activity_main_drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            activity_main_drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
